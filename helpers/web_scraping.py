4f1acc2247290aff7485a2904eca80ea2ae37ae3
Dylan TAN <dylan.tan@upskills.ai>
2024-03-27 16:24:45 +0800

    multiple page posts

--- helpers/web_scraping.py
+++ helpers/web_scraping.py
@@ -1,4 +1,5 @@
 """Helpers for scraping a geekhack page"""
+import requests
 from bs4 import BeautifulSoup
 
 
@@ -22,3 +23,30 @@ def topic_posts_by_page(page_soup: BeautifulSoup):
     subposts = page_soup.find_all("div", class_="inner")
     # return [subpost.text for subpost in subposts]
     return subposts
+
+
+def all_topic_posts(topic_id: int):
+    """Loops all pages of a given topic to obtain
+    all user posts in html block format"""
+
+    topic_fp_url = f"https://geekhack.org/index.php?topic={topic_id}"
+    topic_fp = requests.get(topic_fp_url)
+    topic_fp_soup = BeautifulSoup(topic_fp.content, "html.parser")
+
+    topic_page_no = topic_pages(topic_fp_soup)
+    topic_posts = topic_posts_by_page(topic_fp_soup)
+
+    # gb url has page number as gb_fp_url.00 for page 1
+    # followed by gb_fp_url.50 for page 2
+
+    for i in range(1, topic_page_no):
+        page_counter = i * 0.5
+        topic_page_id = topic_id + page_counter
+        topic_page_url = f"https://geekhack.org/index.php?topic={topic_page_id}0"
+        topic_page = requests.get(topic_page_url)
+        topic_page_soup = BeautifulSoup(topic_page.content, "html.parser")
+
+        topic_page_posts = topic_posts_by_page(topic_page_soup)
+        topic_posts.extend(topic_page_posts)
+
+    return topic_posts
