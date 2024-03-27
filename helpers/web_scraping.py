a5358004012eb1e076d8e9dfc5523bf62f0a9754
Dylan TAN <dylan.tan@upskills.ai>
2024-03-27 16:16:11 +0800

    1 page topics

--- helpers/web_scraping.py
+++ helpers/web_scraping.py
@@ -3,13 +3,18 @@ from bs4 import BeautifulSoup
 
 
 def topic_pages(page_soup: BeautifulSoup):
-    """Obtain number of pages for a given topic"""
+    """Obtain number of pages for a given topic from front page"""
     page_navigation = list(set(page_soup.find_all("a", class_="navPages")))
     page_text_list = [
         navPage.text for navPage in page_navigation if "»" not in navPage.text
     ]
     page_num_list = [int(text) for text in page_text_list]
-    return max(page_num_list)
+
+    # We do the following checks because we do not find the navPages button 1
+    if page_num_list:
+        return max(page_num_list)
+    else:
+        return 1
 
 
 def topic_posts_by_page(page_soup: BeautifulSoup):
