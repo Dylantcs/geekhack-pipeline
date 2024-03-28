7b4f0d4d61b2b4fc5b4ec014221121e257d7c623
Dylan TAN <dylan.tan@upskills.ai>
2024-03-28 15:54:58 +0800

    get parent post text and reply text

--- classes/topic_post.py
+++ classes/topic_post.py
@@ -12,6 +12,8 @@ class TopicPost(object):
         self.tag = user_post
         self.text: str = user_post.text
         self._parent_post_id: List[int] = None
+        self._previous_post_target_text: List[str] = None
+        self._replies: List[str] = None
 
     @property
     def parent_post_id(self):
@@ -19,15 +21,31 @@ class TopicPost(object):
         of another post.
         If there is a reply/ies, then the current post's parent(s) is the post(s)
         it replies to"""
+        # TODO: Reorg this to be a function that updates the 3 attr?
         if not self._parent_post_id:
             parent_post_id: List[int] = []
+            reply_target_text: List[str] = []
+            prev_post_reply: List[str] = []
             replies_from_tag: List[Tag] = self.tag.find_all("div", class_="quoteheader")
 
             if replies_from_tag:
                 for prev_post in replies_from_tag:
+                    # get previous post message id
                     prev_post_url: str = prev_post.find("a").get("href")
                     prev_post_id: int = prev_post_url.split("msg")[-1]
                     parent_post_id.append(prev_post_id)
+
+                    # get text part of post that user is replying to
+                    prev_post_target_text = prev_post.next_sibling.text
+                    reply_target_text.append(prev_post_target_text)
+                    # get text of reply
+                    reply_text = (
+                        prev_post.next_sibling.next_sibling.next_sibling.next_sibling
+                    )
+                    prev_post_reply.append(reply_text)
+
             self._parent_post_id = parent_post_id
+            self._reply_target_text = reply_target_text
+            self._replies = prev_post_reply
 
         return self._parent_post_id
